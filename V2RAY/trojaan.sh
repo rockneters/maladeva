44a5659d4be90254616bee5c37d8a39fa892ba06
rockneters <89226199+rockneters@users.noreply.github.com>
2021-11-06 21:54:08 +0700

    Update trojaan.sh

--- V2RAY/trojaan.sh
+++ V2RAY/trojaan.sh
@@ -3,17 +3,18 @@ source /var/lib/premium-script/settvps.conf
 echo "Permission Accepted"
 sleep 1
 clear
-figlet PANEL TROJAN | lolcat -d 5
-echo -e "╠══════════════════════════════════════════════════════════╣" | lolcat
+figlet PANEL TROJAN | lolcat
+echo -e "════════════════════════════════════════════════════════════" | lolcat
 echo -e "                     ┃V2RAY TROJAN MENU┃                  " |lolcat
-echo -e "╠══════════════════════════════════════════════════════════╣" | lolcat
-echo -e "[1] • Create Trojan Account"
-echo -e "[2] • Deleting Trojan Account"
-echo -e "[3] • Renew Trojan Account"
-echo -e "[4] • Check User Login Trojan"
-echo -e "╠══════════════════════════════════════════════════════════╣" | lolcat
-echo -e "[x] • MENU"
-echo -e "╠══════════════════════════════════════════════════════════╣" | lolcat
+echo -e "════════════════════════════════════════════════════════════" | lolcat
+echo -e " [1] • Create Trojan Account"
+echo -e " [2] • Deleting Trojan Account"
+echo -e " [3] • Renew Trojan Account"
+echo -e " [4] • Check User Login Trojan"
+echo -e " [5] • Trial Trojan Account"
+echo -e "════════════════════════════════════════════════════════════" | lolcat
+echo -e " [x] • MENU"
+echo -e "════════════════════════════════════════════════════════════" | lolcat
 echo -e ""
 read -p "Please Input Number  [ 1-4 or x ] :  "  trojan
 echo -e ""
@@ -35,6 +36,11 @@ trojaan
 ;;
 4)
 cek-tr
+read -p "Tekan ENTER untuk kembali"
+trojaan
+;;
+5)
+trial-tr
 read -p "Tekan ENTER untuk kembali"
 trojaan
 ;;
