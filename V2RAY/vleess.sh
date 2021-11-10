85834b8e176d3f11f4c01fa3d75c0cbb3f5ccc7b
rockneters <89226199+rockneters@users.noreply.github.com>
2021-11-10 18:05:12 +0700

    Update vleess.sh

--- V2RAY/vleess.sh
+++ V2RAY/vleess.sh
@@ -3,17 +3,18 @@ source /var/lib/premium-script/settvps.conf
 echo "Permission Accepted"
 sleep 1
 clear
-figlet PANEL VLESS | lolcat -d 5
-echo -e "╠══════════════════════════════════════════════════════════╣" | lolcat
+figlet PANEL VLESS | lolcat
+echo -e "══════════════════════════════════════════════════════════" | lolcat
 echo -e "                     ┃V2RAY VLESS MENU┃                   " |lolcat
-echo -e "╠══════════════════════════════════════════════════════════╣" | lolcat
-echo -e "[1] • Create Vless Websocket Account"
-echo -e "[2] • Deleting Vless Websocket Account"
-echo -e "[3] • Renew Vless Account "
-echo -e "[4] • Check User Login Vless"
-echo -e "╠══════════════════════════════════════════════════════════╣" | lolcat
-echo -e "[x] • MENU"
-echo -e "╠══════════════════════════════════════════════════════════╣" | lolcat
+echo -e "══════════════════════════════════════════════════════════" | lolcat
+echo -e "  [1] • Create Vless Websocket Account"
+echo -e "  [2] • Deleting Vless Websocket Account"
+echo -e "  [3] • Renew Vless Account "
+echo -e "  [4] • Check User Login Vless"
+echo -e "  [5] • Trial Vless Account"
+echo -e "══════════════════════════════════════════════════════════" | lolcat
+echo -e "  [x] • MENU"
+echo -e "══════════════════════════════════════════════════════════" | lolcat
 echo -e ""
 read -p "Please Input Number  [ 1-4 or x ] :  "  vless
 echo -e ""
@@ -35,6 +36,11 @@ vleess
 ;;
 4)
 cek-vless
+read -p "Tekan ENTER untuk kembali"
+vleess
+;;
+5)
+trial-vless
 read -p "Tekan ENTER untuk kembali"
 vleess
 ;;
