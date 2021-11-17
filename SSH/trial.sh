812b85b29983cf764735eba8e7da736e5b5280dd
rockneters <89226199+rockneters@users.noreply.github.com>
2021-11-18 00:05:13 +0700

    Update trial.sh

--- SSH/trial.sh
+++ SSH/trial.sh
@@ -26,28 +26,36 @@ sleep 0.5
 clear
 useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
 exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
-echo -e "Success!"
-echo -e "===============================" | lolcat
-echo -e "SSH OpenVPN Account Configuration"
-echo -e "===============================" | lolcat
+echo ""
+echo "Success!"
+echo "==========================" | lolcat
+echo " $OWNER " 
+echo "   TRIAL SSH & OpenVPN" 
+echo "==========================" | lolcat
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 echo -e ""
 echo -e "Informasi Trial SSH & OpenVPN"
 echo -e "Username   : $Login "
 echo -e "Password   : $Pass"
-echo -e "===============================" | lolcat
-echo -e "Host           : $domain"
-echo -e "IP Server  : $IP"
-echo -e "Dropbear   : 109, 143"
-echo -e "SSL/TLS    : 443"
-echo -e "OpenVPN    : TCP $ovpn http://$IP:81/client-tcp-$ovpn.ovpn"
-echo -e "OpenVPN    : UDP $ovpn2 http://$IP:81/client-udp-$ovpn2.ovpn"
-echo -e "OpenVPN    : SSL 442 http://$IP:81/client-tcp-ssl.ovpn"
-echo -e "badvpn     : 7100-7300"
-echo -e "===============================" | lolcat
-echo -e "Masa Aktif   : $masaaktif hari"
-echo -e "Aktif Sampai : $exp"
-echo -e "===============================" | lolcat
-echo -e "Terima kasih banyak"
-echo -e "By. Rocknet Store | VPN"
-echo -e ""
+echo -e "Masa Aktif : $masaaktif hari"
+echo -e "Expaired   : $exp"
+echo "==========================" | lolcat
+echo "ISP Server     : $ISP"
+echo "Host           : $domain"
+echo "IP Server      : $IP"
+echo "Dropbear       : 109, 143"
+echo "WEBSOCKET TLS  : $wstls"
+echo "SSL/TLS        : $ssl"
+echo "Squid          : $sqd"
+echo "Badvpn         : $badvpn"
+echo "==========================" | lolcat
+echo "PAYLOAD WS  :"
+echo "GET / HTTP/1.1[crlf]Host: ${domain}[crlf]Upgrade: websocket[crlf][crlf]"
+echo "==========================" | lolcat
+echo "Download FIle OpenVPN"
+echo "TCP $ovpn   : http://$IP:81/client-tcp-$ovpn.ovpn"
+echo "UDP $ovpn2  : http://$IP:81/client-udp-$ovpn2.ovpn"
+echo "SSL $ssl    : http://$IP:81/client-tcp-ssl.ovpn"
+echo "=========================" | lolcat
+echo "${PESAN}"
+echo ""
