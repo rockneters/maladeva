481d4a8af552e7312f90780ab2e49c6239cb9d7f
rockneters <89226199+rockneters@users.noreply.github.com>
2021-11-17 23:41:38 +0700

    Update trial-tr.sh

--- V2RAY/trial-tr.sh
+++ V2RAY/trial-tr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-source /var/lib/premium-script/settvps.conf
 clear
+source /var/lib/premium-script/settvps.conf
 echo "Permission Accepted"
 sleep 1
 clear
@@ -17,8 +17,13 @@ sed -i '/"'""$uuid""'"$/a\,"'""$user""'"' /etc/trojan/config.json
 echo -e "### $user $exp" >> /etc/trojan/akun.conf
 systemctl restart trojan
 trojanlink="trojan://${user}@${domain}:${tr}"
+echo "#TROJAN $user $exp" >> /etc/akun.conf
+echo ""
+read -p "Select 1 for Non-GCP and 2 for GCP :  " pilih
+echo -e ""
+case $pilih in
+1)
 clear
-
 echo "Success!"
 echo "==========================" | lolcat
 echo "${OWNER}"
@@ -36,3 +41,29 @@ echo "Expaired   : $exp WIB"
 echo "==========================" | lolcat
 echo "${PESAN}"
 echo ""
+;;
+2)
+read -p "Sub Domain GCP : " dgcp
+read -p "Port GCP : " pgcp
+clear
+echo "Success!"
+echo "==========================" | lolcat
+echo "${OWNER}"
+echo "        • TROJAN •        "
+echo "==========================" | lolcat
+echo "Remarks    : ${user}"
+echo "Host       : ${dgcp}"
+echo "port       : ${pgcp}"
+echo "Key        : ${user}"
+echo "link       : trojan://${user}@${dgcp}:${pgcp}"
+echo "==========================" | lolcat
+echo "Masa Aktif : $masaaktif Jam"
+echo "Expaired   : $exp WIB"
+echo "==========================" | lolcat
+echo "${PESAN}"
+echo ""
+;;
+*)
+read -p "Select 1 for Non-GCP and 2 for GCP :  " pilih
+;;
+esac
