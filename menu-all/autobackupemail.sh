c626a5b430106adc1beac279291f17fa8d7709d6
rockneters <89226199+rockneters@users.noreply.github.com>
2021-11-18 15:56:55 +0700

    Update autobackupemail.sh

--- menu-all/autobackupemail.sh
+++ menu-all/autobackupemail.sh
@@ -5,26 +5,7 @@ BLUE='\e[0;34m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
 echo -e  "${RED}Checking VPS${NC}"
-sleep 2
-IZIN=$( curl https://raw.githubusercontent.com/dpvpn09/ipvps/main/ipvps | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${GREEN}Permission Accepted...${NC}"
-sleep 2 
-else
-clear
-echo -e ""
-echo -e "======================================="
-echo -e ""
-echo -e "${RED}Permission Denied...!!! ${NC}"
-echo -e "IP VPS ANDA BELUM TERDAFTAR"
-echo -e "Contact WA https//wa.me/+6281285970456"
-echo -e "For Registration IP VPS"
-echo -e ""
-echo -e "======================================="
-echo -e ""
-rm setup.sh
-exit 0
-fi
+sleep 1
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[ON]${Font_color_suffix}"
 Error="${Red_font_prefix}[OFF]${Font_color_suffix}"
